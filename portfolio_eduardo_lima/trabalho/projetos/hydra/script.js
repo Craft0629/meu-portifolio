c7d35d59c9492b6cdde2e9543491c0dd0a7ee76e
eduardo lima <eduardolimapereiradasilva@gmail.com>
2026-09-23 14:09:28 -0300

    Delete portfolio_eduardo_lima directory

--- portfolio_eduardo_lima/trabalho/projetos/hydra/script.js
+++
@@ -1,8 +0,0 @@
-
-const elementos = document.querySelectorAll(".reveal");
-const observer = new IntersectionObserver((entradas) => {
-    entradas.forEach((entrada) => {
-        if (entrada.isIntersecting) entrada.target.classList.add("visible");
-    });
-}, { threshold: 0.12 });
-elementos.forEach((elemento) => observer.observe(elemento));
